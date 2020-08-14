7202fcc9037073439c82e90bc1bdc4014bdcb7aa
Ulyana <ubychkovskaya20@gmail.com>
2020-08-14 18:01:11 +0300

    feat: add build folder to node js

--- api/app.js
+++ api/app.js
@@ -1,17 +1,23 @@
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const routes = require('../api/routes/index');
+const path = require('path')
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const path = {
+const paths = {
   api: '/api',
 }
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(path.api, routes);
+app.use(paths.api, routes);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 9000;
 app.listen(port);
--- client/src/components/routing/routing.js
+++ client/src/components/routing/routing.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,  // BrowserRouter
   Switch,
   Route,
   Link
