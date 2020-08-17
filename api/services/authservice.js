68625f47ce412392155282f2f0ee258aeccdd061
Ulyana <ubychkovskaya20@gmail.com>
2020-08-17 14:37:16 +0300

    feat: changed then to async/await

--- api/services/authservice.js
+++ api/services/authservice.js
@@ -11,23 +11,22 @@ class AuthService {
         this.accessTokenSecret = config.accessToken;
     }
 
-    signUp(name, email, password) {
+    async signUp(name, email, password) {
         const passwordHashed = this.hashPassword(password);
 
-        pool.query('SELECT * FROM users WHERE email = $1', [email])  // async await
-            .then(result =>{ 
-                const isResultNotEmpty = result.rows.length !== 0;
+        try {
+            const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+            const isResultNotEmpty = result.rows.length !== 0;
 
-                if (isResultNotEmpty) {
-                    throw new Error;
-                }
-                pool.query('INSERT INTO users (email, password, name) VALUES ($1, $2, $3)',
-                    [email, passwordHashed, name]
-                );
-            })
-            .catch(function(err) {
-                return new Error(SIGNUPERROR);
-            });
+            if (isResultNotEmpty) {
+                throw new Error;
+            }
+            pool.query('INSERT INTO users (email, password, name) VALUES ($1, $2, $3)',
+                [email, passwordHashed, name]
+            );
+        } catch(err) {
+            return new Error(SIGNUPERROR);
+        }
 
         const accessToken = jwt.sign({ name, email }, this.accessTokenSecret);
 
@@ -39,20 +38,21 @@ class AuthService {
 
     logIn(email, password) {
         let name;
-        pool.query('SELECT * FROM users WHERE email = $1', [email])
-            .then(result => {
-                const isResultNotEmpty = result.rows.length !== 0;
-                const passwordHashed = this.hashPassword(password);
-                const isPasswordNotRight = result.rows[0].password !== passwordHashed;
+        try {
+            const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
 
-                if (isResultNotEmpty || isPasswordNotRight) {
-                    throw new Error;
-                }
-                ({ name } = result.rows[0]);
-            })
-            .catch(function(err) {
-                return new Error(LOGINERROR);
-            });
+            const isResultNotEmpty = result.rows.length !== 0;
+            const passwordHashed = this.hashPassword(password);
+            const isPasswordNotRight = result.rows[0].password !== passwordHashed;
+
+            if (isResultNotEmpty || isPasswordNotRight) {
+                throw new Error;
+            }
+            ({ name } = result.rows[0]);
+
+        } catch(err) {
+            return new Error(LOGINERROR);
+        }
 
         const accessToken = jwt.sign({ user: name, email }, this.accessTokenSecret);
 
