dfc729ad1a7ef5d01377bf915329c3e51c24f174
Ulyana <ubychkovskaya20@gmail.com>
2020-08-20 10:29:51 +0300

    fix: fix bugs with build folder

---
+++ client/build/precache-manifest.686cf472646582ed385afbb69feaad32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06491ef8c32d598ddc25a22291b3e772",
+    "url": "./index.html"
+  },
+  {
+    "revision": "effc706bb50f65d60299",
+    "url": "./static/css/main.96e423d4.chunk.css"
+  },
+  {
+    "revision": "501a5337ab764a60ae9f",
+    "url": "./static/js/2.43e8069e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.43e8069e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "effc706bb50f65d60299",
+    "url": "./static/js/main.5debb1c3.chunk.js"
+  },
+  {
+    "revision": "c349d31f5c89e8eaff6c",
+    "url": "./static/js/runtime-main.0c71ff7d.js"
+  }
+]);
