8535508cbda703588804ba9797e6a58ba6c77c99
Ulyana <ubychkovskaya20@gmail.com>
2020-08-25 14:47:40 +0300

    fix: fix bugs

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(paths.api, routes);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
