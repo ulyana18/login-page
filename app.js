18e83c97bf7eb1d128c15d4bcf451e9b97eb799a
Ulyana <ubychkovskaya20@gmail.com>
2020-09-02 10:56:04 +0300

    feat: add chat

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
+const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
 if (process.env.NODE_ENV === 'test') {
