09f0704b823917d5af44e986df15fb0aa9c41e2d
Ulyana <ubychkovskaya20@gmail.com>
2020-09-02 14:52:15 +0300

    feat: add build folder

--- build/precache-manifest.1d736338588add255060f9300de89c9c.js
+++ build/precache-manifest.1d736338588add255060f9300de89c9c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcdc949982fe6ec79143da54fb7ad6b0",
+    "revision": "59bf9c3f6ae84aebbe82b59aba080ffe",
     "url": "/index.html"
   },
   {
-    "revision": "a33e08817d7d54f8d4ab",
+    "revision": "6c9ea01bc19eabb17b08",
     "url": "/static/css/main.5c57bcf2.chunk.css"
   },
   {
-    "revision": "e91c71b02792482709df",
-    "url": "/static/js/2.3b11056c.chunk.js"
+    "revision": "7fe0ea494e9761d21229",
+    "url": "/static/js/2.87e161f2.chunk.js"
   },
   {
     "revision": "6b35d1fa9f1c5a91f1f6eb43892a03d9",
-    "url": "/static/js/2.3b11056c.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.87e161f2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a33e08817d7d54f8d4ab",
-    "url": "/static/js/main.c432640f.chunk.js"
+    "revision": "6c9ea01bc19eabb17b08",
+    "url": "/static/js/main.140ebc23.chunk.js"
   },
   {
     "revision": "3b290088996343aaf4db",
