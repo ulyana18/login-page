bea66f707b7b350fb9a9c60a1ef1ddacb8663b7b
Ulyana <ubychkovskaya20@gmail.com>
2020-09-02 10:48:37 +0300

    feat: add chat

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const bodyParser = require('body-parser');
 require('dotenv').config();
-const path = require('path')
+const path = require('path');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+if (process.env.NODE_ENV === 'test') {
+  const process = require('process');
+  process.chdir(process.env.NODE_PATH);
+}
 
 const routes = require('./routes/index');
 const paths = require('./routes/paths');
-const config = require('./config');
 const pool = require('./db/queries');
 
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));  // , 'client/build'
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(paths.api, routes);
@@ -21,8 +27,28 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+const port = process.env.PORT || 9000;
 
-const port = process.env.PORT || 9000;  
-app.listen(port);
+io.on('connection', async socket => {
+  const { id } = socket.client;
+  console.log(`User connected: ${id}`);
+
+  socket.on('chat message', async (message, name, email) => {
+    console.log(`${email}: ${message}`);
+
+    await pool.query('INSERT INTO chat (message, name, email) VALUES ($1, $2, $3)',
+      [message, name, email]
+    );
+
+    io.emit('chat message', { message, name, email });
+  });
+  
+  socket.on('get database', async () => {
+    const result = await pool.query('SELECT * FROM chat');
+    io.emit('get database', result.rows);
+  });
+});
+
+server.listen(port);
 
 module.exports = app;
