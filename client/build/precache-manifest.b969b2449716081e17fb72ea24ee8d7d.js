ca6e6ed0110d80b7f63d995d8bf3a50220691b77
Ulyana <ubychkovskaya20@gmail.com>
2020-09-02 14:22:44 +0300

    feat: add build folder

--- client/build/precache-manifest.b969b2449716081e17fb72ea24ee8d7d.js
+++ client/build/precache-manifest.b969b2449716081e17fb72ea24ee8d7d.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e4869e4700687dc5c347049b74856a7d",
+    "revision": "dcdc949982fe6ec79143da54fb7ad6b0",
     "url": "/index.html"
   },
   {
-    "revision": "aa19d2aa657571ffea02",
+    "revision": "a33e08817d7d54f8d4ab",
     "url": "/static/css/main.5c57bcf2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.3b11056c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aa19d2aa657571ffea02",
-    "url": "/static/js/main.26695e31.chunk.js"
+    "revision": "a33e08817d7d54f8d4ab",
+    "url": "/static/js/main.c432640f.chunk.js"
   },
   {
     "revision": "3b290088996343aaf4db",
