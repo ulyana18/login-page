98f88ce88f7ac25e088ce0661d669530e43e163b
Ulyana <ubychkovskaya20@gmail.com>
2020-09-11 10:13:20 +0300

    feat: add search

---
+++ build/precache-manifest.e32b5c43deca5fa645b1915f183b7592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a345aef52d08cd90157fe78d0032bc7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12131dad3fe1c6c68eda",
+    "url": "/static/css/main.5fddb292.chunk.css"
+  },
+  {
+    "revision": "a841075bb9552b4ed503",
+    "url": "/static/js/2.50acc012.chunk.js"
+  },
+  {
+    "revision": "6b35d1fa9f1c5a91f1f6eb43892a03d9",
+    "url": "/static/js/2.50acc012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12131dad3fe1c6c68eda",
+    "url": "/static/js/main.86654569.chunk.js"
+  },
+  {
+    "revision": "3b290088996343aaf4db",
+    "url": "/static/js/runtime-main.9fd96ad0.js"
+  }
+]);
