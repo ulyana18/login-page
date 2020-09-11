43b24acab333e283b4ba4a3b31f79aef7e213410
Ulyana <ubychkovskaya20@gmail.com>
2020-09-11 10:31:33 +0300

    fix: fix bugs

--- api/routes/signup.js
+++ api/routes/signup.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
-const AuthService = require('../services/authService');
-const { SUCCESSFULL, ERROR } = require('../messages/consts');
-const { SIGNUP_ERROR } = require('../messages/userMessages');
+const AuthService = require('services/authService');
+const { SUCCESSFULL, ERROR } = require('messages/consts');
+const { SIGNUP_ERROR } = require('messages/userMessages');
 
 
 async function signup(req, res) {
